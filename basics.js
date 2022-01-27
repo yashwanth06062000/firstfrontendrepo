a85827fc36316e5b0c376c5f6504931595892f7e
YASHWANTH KRISHNA BUSETTY <yashwanthkrishnabusetty@YASHWANTHs-MacBook-Air.local>
2022-01-27 10:48:01 +0530

    added basics.js

---
+++ basics.js
@@ -0,0 +1,12 @@
+// function Console(event) {
+//     event.preventDefault();
+// var data = document.getElementById("name");
+// var email=document.getElementById("email")
+// var phone=document.getElementById("phone")
+// var date=document.getElementById("timeforcall")
+// console.log(data.value);
+// console.log(email.value);
+// console.log(phone.value);
+// console.log(date.value);
+// }
+console.log('yashwanth')
