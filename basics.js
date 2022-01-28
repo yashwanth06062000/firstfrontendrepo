8e5bbba82288c8c8d4f8911abfe5992cb0d98a52
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-01-28 13:42:57 +0530

    parent child sibling added new div

--- basics.js
+++ basics.js
@@ -21,11 +21,41 @@
 // seconditem.style.backgroundColor='green';
 // let thirditem=document.querySelector('li:nth-child(3)');
 // thirditem.style.display='none';
-let items=document.querySelectorAll('.it');
-items[1].style.color='green';
-let odd=document.querySelectorAll('li:nth-child(odd)');
-console.log(odd)
-for(let i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+// let items=document.querySelectorAll('.it');
+// items[1].style.color='green';
+// let odd=document.querySelectorAll('li:nth-child(odd)');
+// console.log(odd)
+// for(let i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
+// let play=document.querySelector('.it')
+// console.log(play.parentElement.setAttribute('title','uilist'))
+
+// console.log(play.parentNode)
+// let play=document.querySelector('.formbox')
+// console.log(play.childNodes)
+// console.log(play.children)
+// console.log(play.firstChild)
+// console.log(play.firstElementChild)
+// console.log(play.lastChild)
+// console.log(play.lastElementChild)
+// let play=document.querySelector('.innerform')
+// console.log(play.nextSibling)
+// console.log(play.nextElementSibling)
+// console.log(play.previousSibling)
+// console.log(play.previousElementSibling)
+let newDiv=document.createElement('div')
+newDiv.setAttribute('title','newdiv')
+newDiv.className='created';
+newDiv.id='createdid';
+newtextnode=document.createTextNode('Hello Dude1')
+newDiv.appendChild(newtextnode);
+let tt=document.querySelector('.formbox')
+console.log(tt)
+let ff=document.querySelector('.innerform')
+console.log(tt)
+tt.insertBefore(newDiv,ff)
+console.log(newDiv);
+
+
 
