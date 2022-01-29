c0d8d865c301b0aebc68e84149d1c15f26224c81
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-01-29 12:54:16 +0530

    form which takes the datails and adds to local storage

--- basics.js
+++ basics.js
@@ -56,88 +56,102 @@
 // console.log(tt)
 // tt.insertBefore(newDiv,ff)
 // console.log(newDiv);
-let btn=document.querySelector('.but')
-let value=document.getElementById('Addingitems')
-btn.addEventListener('click',addito)
-function addito(e){
-    e.preventDefault();
-    let value2=document.getElementById('AddingItems').value;
-    let value1=document.getElementById('discription').value;
+// let btn=document.querySelector('.but')
+// let value=document.getElementById('Addingitems')
+// btn.addEventListener('click',addito)
+// function addito(e){
+//     e.preventDefault();
+//     let value2=document.getElementById('AddingItems').value;
+//     let value1=document.getElementById('discription').value;
     
-    let pil=document.createElement('p')
-    pil.className='pt';
-    pil.append(document.createTextNode(value1))
-    let rp=document.createElement('li')
-    rp.className='it'
-    rp.id='ibtnc'
-    let ttt=document.createTextNode(value2)
-    rp.append(ttt)
-    rp.append(pil);
-    let bty=document.createElement('button')
-    bty.append(document.createTextNode('x'))
-    bty.className='ibtn';
-    rp.append(bty)
-    let ull=document.querySelector('.items')
-    ull.appendChild(rp);
-}
-//removing the added item in list
-let rem=document.querySelector('.items')
+//     let pil=document.createElement('p')
+//     pil.className='pt';
+//     pil.append(document.createTextNode(value1))
+//     let rp=document.createElement('li')
+//     rp.className='it'
+//     rp.id='ibtnc'
+//     let ttt=document.createTextNode(value2)
+//     rp.append(ttt)
+//     rp.append(pil);
+//     let bty=document.createElement('button')
+//     bty.append(document.createTextNode('x'))
+//     bty.className='ibtn';
+//     rp.append(bty)
+//     let ull=document.querySelector('.items')
+//     ull.appendChild(rp);
+// }
+// //removing the added item in list
+// let rem=document.querySelector('.items')
 
-rem.addEventListener('click',remo)
-function remo(e){
-    if(e.target.className=='ibtn'){
-        if(confirm('Are You Sure?')){
-            var li = e.target.parentElement;
-            rem.removeChild(li);
-          }
-    }
-}
-let search=document.getElementById('searchitems')
-let searchdis=document.getElementById('searchdiscription')
+// rem.addEventListener('click',remo)
+// function remo(e){
+//     if(e.target.className=='ibtn'){
+//         if(confirm('Are You Sure?')){
+//             var li = e.target.parentElement;
+//             rem.removeChild(li);
+//           }
+//     }
+// }
+// let search=document.getElementById('searchitems')
+// let searchdis=document.getElementById('searchdiscription')
 
-search.addEventListener('keyup',ser)
-searchdis.addEventListener('keyup',ser1)
-let flag=[];
+// search.addEventListener('keyup',ser)
+// searchdis.addEventListener('keyup',ser1)
+// let flag='hie';
 
-function ser(e){
+// function ser(e){
     
-    let tex=e.target.value.toLowerCase();
-    let r=rem.children;
-    Array.from(r).forEach(function(iu){
-        let rrr=iu.firstChild.textContent.toLowerCase();
-        if(rrr.indexOf(tex)!=-1){
-            flag.push(iu)
-            iu.style.display='block';
+// let tex=e.target.value.toLowerCase();
+
+//     let r=rem.children;
+//     Array.from(r).forEach(function(iu){
+//         let rrr=iu.firstChild.textContent.toLowerCase();
+//         if(rrr.indexOf(tex)!=-1){
+//             flag.push(iu)
+//             iu.style.display='block';
             
-        }
-        else{
-            iu.style.display='none';
+//         }
+//         else{
+//             iu.style.display='none';
 
-        }
-    })
-}
-function ser1(e){
-    console.log("2")
-    let tex=e.target.value.toLowerCase();
-    let r=rem.children;
-    Array.from(r).forEach(function(iu){
+//         }
+//     })
+// }
+// function ser1(e){
+//     console.log("2")
+//     let tex=e.target.value.toLowerCase();
+//     let r=rem.children;
+//     Array.from(r).forEach(function(iu){
         
-        let rrr=iu.querySelector('.pt').textContent.toLowerCase();
-        if(rrr.indexOf(tex)!=-1){
-            let flag1=0;
-            for(let i=0;i<flag.length;i++){
-                if(flag[i]==iu){
-                    flag1=1;
-                }
-            }
-            if(flag1==1){
-            iu.style.display='block';}
-        }
-        else{
-            iu.style.display='none';
-        }
-    })
+//         let rrr=iu.querySelector('.pt').textContent.toLowerCase();
+//         if(rrr.indexOf(tex)!=-1){
+//             let flag1=0;
+//             for(let i=0;i<flag.length;i++){
+//                 if(flag[i]==iu){
+//                     flag1=1;
+//                 }
+//             }
+//             if(flag1==1){
+//             iu.style.display='block';}
+//         }
+//         else{
+//             iu.style.display='none';
+//         }
+//     })
     
+// }
+
+let func=document.getElementById('but')
+func.addEventListener('click',call);
+function call(e){
+e.preventDefault();
+let name1=document.getElementById('name').value;
+let email=document.getElementById('email').value;
+let phone=document.getElementById('phone').value;
+let date=document.getElementById('timeforcall').value;
+localStorage.setItem('name',name1);
+localStorage.setItem('email',email);
+localStorage.setItem('phone',phone);
+localStorage.setItem('date',date);
+
 }
-
-
