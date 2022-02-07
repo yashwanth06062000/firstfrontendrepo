255b03e98c017fad38910c3ef391ee584bea9511
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-02-07 15:42:42 +0530

    adding and  showing on screen from crudcrud

--- curdcurdapp.js
+++ curdcurdapp.js
@@ -3,10 +3,35 @@ let btn=document.querySelector('.but')
 let value=document.getElementById('name')
 let counte=0;
 let ramu=document.querySelector('.items');
-btn.addEventListener('click',addito)
 btn.addEventListener('click',addingtocurdcurd)
+window.addEventListener('DOMContentLoaded', (event) => {
+    axios
+    .get("https://crudcrud.com/api/2f4d01999b8e4c6480b7aadd471a18d4/practicedata").then((res)=>{
+        for(let i=0;i<res.data.length;i++){
+            displayyallusersfromcurdcurd(res.data[i])
+        }
+    })
+
+});
+function displayyallusersfromcurdcurd(user){
+    let name=user.name;
+    let newli=document.createElement('li')
+    newli.className='it'
+    newli.id='ibtnc'
+    let textnode=document.createTextNode(name)
+    newli.append(textnode)
+    let newbutton=document.createElement('button')
+    newbutton.append(document.createTextNode('x'))
+    newbutton.className='ibtn';
+    newli.append(newbutton)
+    let existinglist=document.querySelector('.items')
+    existinglist.appendChild(newli);
+
+
+}
 
 function addingtocurdcurd(e){
+    e.preventDefault()
     let name=document.getElementById('name').value;
     let email=document.getElementById('email').value;
     let phoneno=document.getElementById('phone').value;
@@ -15,25 +40,10 @@ function addingtocurdcurd(e){
         email:email,
         phone:phoneno
     }
-    console.log(obj)
-    axios.post("https://crudcrud.com/api/2f4d01999b8e4c6480b7aadd471a18d4/practicedata",obj)
-
-
-}
-function addito(e){
-    e.preventDefault();
-    let value=document.getElementById('name').value;
-    let rp=document.createElement('li')
-    rp.className='it'
-    rp.id='ibtnc'
-    let ttt=document.createTextNode(value)
-    rp.append(ttt)
-    let bty=document.createElement('button')
-    bty.append(document.createTextNode('x'))
-    bty.className='ibtn';
-    rp.append(bty)
-    let ull=document.querySelector('.items')
-    ull.appendChild(rp);
+    console.log("entered")
+    axios
+    .post("https://crudcrud.com/api/2f4d01999b8e4c6480b7aadd471a18d4/practicedata",obj)
+    .then((err)=>{displayyallusersfromcurdcurd(err.data)})
 }
 let rem=document.querySelector('.items')
 
