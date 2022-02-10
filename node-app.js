5b440c83cd0d18ec7ba4f145f18da596adfb3735
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-02-10 17:34:25 +0530

    created 3 sites based on request url it has to open that pages

--- node-app.js
+++ node-app.js
@@ -1,5 +1,46 @@
 const http=require('http')
 const sever=http.createServer((req,res)=>{
-    console.log(req)
+    if(req.url==='/'){
+    res.write('<html>')
+    res.write('<body>')
+    res.write('<form method="POST" action="/message"><input type="text" name="message"><button type="submit">send</button></form>' )
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()
+
+    }
+    if(req.url==='/home'){
+        res.write('<html>')
+        res.write('<body>')
+        res.write(`<h1>welcome home</h1>`)
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    
+    }
+    else if(req.url==='/about'){
+        res.write('<html>')
+        res.write('<body>')
+        res.write(`<h1>welcome to about us page</h1>`)
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    
+    }
+    else if(req.url==='/node'){
+        res.write('<html>')
+        res.write('<body>')
+        res.write(`<h1>welcome to my node js project</h1>`)
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    
+    }
+    res.write('<html>')
+    res.write('<body>')
+    res.write(`<h1>${req.message}</h1>`)
+    res.write('</body>')
+    res.write('</html>')
+    res.end()
 })
-sever.listen(3000)
+sever.listen(4000)
