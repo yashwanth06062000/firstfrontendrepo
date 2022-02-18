ad576789b65066e350c03cdab670705c7aeecced
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-02-18 23:16:28 +0530

    Done

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express=require('express')
 const bodyparser=require('body-parser')
 const adminroutes=require("./routes/admin.js")
 const shoproutes=require("./routes/shop.js")
+const path=require('path')
 
 app=express()
 //doubt should be asked to yash bhaiya that how body parser work and is it okay to put in admin.js
 app.use(bodyparser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin',adminroutes)
-app.use('/shop',shoproutes)
+app.use(shoproutes)
 
 
 
 app.use((req,res,next)=>{
-    res.status(404).send("<h1>page not found</h1>")
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"))
 })
 
 
