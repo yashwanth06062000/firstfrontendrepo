7d83c752153f00202fba01cdec83ec9bd8396c93
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-02-24 14:02:23 +0530

    image caurosal

--- pro.js
+++ pro.js
@@ -1,5 +1,17 @@
-const toggle = document.getElementById("toggle");
+const imgs = document.getElementById("imgs");
 
-toggle.addEventListener("change", (e) => {
-    document.body.classList.toggle("dark", e.target.checked);
-});
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+
+function run() {
+    idx++;
+
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(run, 2000);
