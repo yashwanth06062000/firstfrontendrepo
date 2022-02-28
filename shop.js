a4b90cc3eb8dd804b2d47c53b73338524366f86d
yashwanth06062000 <98481592+yashwanth06062000@users.noreply.github.com>
2022-03-01 00:37:42 +0530

    Adding pagination

--- shop.js
+++ shop.js
@@ -1,25 +1,175 @@
-const path = require('path');
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+const items_per_page=3;
 
-const express = require('express');
+exports.getProducts = (req, res, next) => {
+  const page=+req.query.page || 1;
+  var totalitems=0;
+  
+Product.findAll() //it is not working but 
+  .then(numproducts=>{
+    totalitems=4;
+    return Product.findAll({offset:(page-1)*items_per_page,
+      limit:items_per_page})
+  })
+  // ------------------
+  // i want to send this data to front end to use for page numbers
+  
 
-const shopController = require('../controllers/shop');
 
-const router = express.Router();
+  //====================
+  
+    
+    .then(products => {
+     
+  const hasnextpage=items_per_page*page<totalitems;
+  const haspreviouspage=page>1;
+  const nextpage=page+1;
+  const previouspage=page-1;
+  const lastpage=Math.ceil(totalitems/items_per_page)
+  const obj={
+    totalitems:totalitems,
+    currentpage:page,
+    hasnextpage:hasnextpage,
+    haspreviouspage:haspreviouspage,
+    nextpage:nextpage,
+    previouspage:previouspage,
+    lastpage:lastpage
+  
+  }
+      res.json({products ,success:true,obj})
+      // res.render('shop/product-list', {
+      //   prods: products,
+      //   pageTitle: 'All Products',
+      //   path: '/products'
+      // });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-router.get('/', shopController.getIndex);
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
 
-router.get('/products', shopController.getProducts);
+exports.getIndex = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-router.get('/products/:productId', shopController.getProduct);
+exports.getCart = (req, res, next) => {
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart
+        .getProducts()
+        .then(products => {
+          res.json({products ,success:true})
 
-router.get('/cart', shopController.getCart);
+          // res.render('shop/cart', {
+          //   path: '/cart',
+          //   pageTitle: 'Your Cart',
+          //   products: products
+          // });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+};
 
-router.post('/cart', shopController.postCart);
+exports.postCart = (req, res, next) => {
+  console.log("hie i am post cart called")
+  const prodId = req.body.productID;
+  console.log(req.body.productID)
+  console.log(req.body)
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
 
-router.get('/orders', shopController.getOrders);
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
 
-router.get('/checkout', shopController.getCheckout);
 
-module.exports = router;
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
